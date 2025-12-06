f7a9e9dd8554b61cb6a2b617a2f6938ea2ef45fd
AureliaBett <cbettaurelia@gmail.com>
2025-12-06 04:14:45 +0300

    Set Up of django and django frameworks, Creation of a database, configured PostgreSQL and setup of model

---
+++ CapstoneBackEnd/README.md
@@ -0,0 +1,5 @@
+# uninterrUp (UPS Management and Tracking System)
+
+uninterrUp is a bakcend powered system designed to help workshop teams manage, track and document the repair lifecycle of UPSs from the minute they are received in the workshop to when they are returned to the client/
+## 06/11/2025
+Set up of django and djangoframeworks, Created models in models.py, a database in postgresql, performed migrations.
---
+++ CapstoneBackEnd/api/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+   ROLES = (
+       ('manager', 'Workshop Manager '),  
+         ('engineer', 'Bench Engineer'),
+         ('technician', 'Field Technician'),)
+   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+   
+   role = models.CharField(
+        max_length=20,
+        choices=ROLES,
+        default='technician'
+    )
+   def __str__(self):
+        return f"{self.user.username} - {self.role}"
+   
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    contract =models.CharField(max_length=100, default="Satisfactory")  
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class UPS(models.Model):
+    STATUS = (
+        ('received', 'Received'),
+        ('diagnosis', 'Diagnosis'),
+        ('repairing', 'Repairing'),
+        ('completed', 'Completed'),
+        ('released', 'Released'),
+
+)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="ups_units")
+    serial_number = models.CharField(max_length=100, unique=True)
+    model = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS, default='received')
+    received_date = models.DateTimeField(auto_now_add=True)
+
+class RepairAssignment(models.Model):
+    ups = models.ForeignKey(UPS, on_delete=models.CASCADE, related_name="assignments")
+    engineer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assigned_repairs")
+    assigned_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Assignment: {self.ups.serial_number} → {self.engineer.username}"
+
+
+class RepairUpdate(models.Model):
+    assignment = models.ForeignKey(RepairAssignment, on_delete=models.CASCADE, related_name="updates")
+    notes = models.TextField()
+    attached_file = models.FileField(upload_to="repair_files/", blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Update for {self.assignment.ups.serial_number}"
